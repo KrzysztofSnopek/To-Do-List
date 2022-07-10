6ad17d4dfe4dee5847480b5f7eb1be350a2a1b52
KrzysztofSnopek <108864848+KrzysztofSnopek@users.noreply.github.com>
2022-07-10 17:13:52 +0200

    Adds form

--- to-do-list/src/NewToDoForm.js
+++ to-do-list/src/NewToDoForm.js
@@ -1,22 +1,57 @@
 import React, {Component} from "react";
 
 class NewToDoForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            todos: [{activity: ''}]
+        };
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+    }
+
+    handleSubmit(e){
+        e.preventDefault();
+        this.setState({todos: [...this.state.todos, newToDo]});
+        this.setState({activity: ''});
+    }
+
+    handleChange(e){
+        this.setState({
+            [e.target.name]: e.target.value
+        });
+    }
+
     render () {
-        return (<form>
-            <label>
-                <p>
-                    What do you have to do?
-                </p>
-                <input 
-                type="text"
-                placeholder="Add a new to do"
-                />
-            </label>
-            <input
-            type="submit"
-            value="Send"
-            />
-        </form>
+        return (<div>
+            <form onSubmit={this.handleSubmit}>
+              <label htmlFor="task">
+                  <p>
+                      What do you have to do?
+                  </p>
+              </label>
+              <input
+                   type="text"
+                   value={this.state.activity}
+                   onChange={this.handleChange}
+                   name="activity"
+                   htmlFor='task'
+                   id='task'
+              />
+              <button>
+                Add new task
+              </button>
+           </form>
+          <ul>
+          {this.state.todos.map((todo) => (
+              <li>
+                  {todo.activity}
+              </li>
+          )
+          )}
+          </ul>
+        </div>
         );
     }
 }
